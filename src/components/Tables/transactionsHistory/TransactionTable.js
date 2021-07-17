3399acdfa644d4441d8a8042bd2aff05b02855f7
Hasnain Ali Baloch <hasnainalibaloch@outlook.com>
2021-07-17 16:28:11 +0500

    datatable update

--- src/components/Tables/transactionsHistory/TransactionTable.js
+++ src/components/Tables/transactionsHistory/TransactionTable.js
@@ -1,31 +1,65 @@
 import React from "react";
 import "./transaction.css";
-import DataTable, { createTheme } from 'react-data-table-component';
-import data from './data.json';
+import DataTable, {
+  createTheme,
+  defaultThemes,
+} from "react-data-table-component";
+import data from "./data.json";
 
 import SearchBar from "../../searchBar/SearchBar";
 
-createTheme('solarized', {
+createTheme("dark", {
   text: {
-    primary: '#FFFFFF',
-    secondary: '#FFFFFF',
+    primary: "#FFFFFF",
+    secondary: "#FFFFFF",
+  },
+  headCells: {
+    color: "#878FBC",
   },
   background: {
-    default: '#343c63',
+    default: "#343c63",
   },
   context: {
-    background: '#558BED',
-    text: '#FFFFFF',
+    background: "#558BED",
+    text: "#FFFFFF",
   },
   divider: {
-    default: 'rgba(135, 143, 188, 0.2);',
+    default: "rgba(135, 143, 188, 0.2);",
   },
   action: {
-    button: 'rgba(0,0,0,.54)',
-    hover: '#558BED',
-    disabled: 'rgba(0,0,0,.12)',
+    button: "rgba(0,0,0,.54)",
+    hover: "#558BED",
+    disabled: "rgba(0,0,0,.12)",
   },
 });
+
+const customStyle = {
+  header: {
+    style: {
+      minHeight: "10px",
+    },
+  },
+  headRow: {
+    style: {
+      color: "#558BED",
+    },
+  },
+  headCells: {
+    style: {
+      color: "#878FBC",
+      fontSize: '16px',
+      hover:{
+        color: "#ffffff"
+      }
+    },
+  },
+  cells: {
+    style: {
+        color: '#ffffff',
+        fontSize: '14px'
+      },
+  },
+};
 
 export default function TransactionTable() {
   const columns = [
@@ -47,10 +81,12 @@ export default function TransactionTable() {
     {
       name: "Value",
       selector: "value",
+      sortable: true,
     },
     {
       name: "Type",
       selector: "type",
+      sortable: true,
     },
   ];
 
@@ -64,9 +100,11 @@ export default function TransactionTable() {
       </div>
       <DataTable
         columns={columns}
+        pointerOnHover
         data={data}
-        highlightOnHover
-        theme={"solarized"}
+        responsive
+        theme={"dark"}
+        customStyles={customStyle}
         pagination
         paginationPerPage={5}
         paginationRowsPerPageOptions={[5, 10, 20]}
