4cfa6b588fd047db4f7b413e7d8f6f28a1c72894
Hasnain Ali Baloch <hasnainalibaloch@outlook.com>
2021-07-17 15:58:18 +0500

    datatable

--- src/components/Tables/transactionsHistory/TransactionTable.js
+++ src/components/Tables/transactionsHistory/TransactionTable.js
@@ -1,9 +1,59 @@
 import React from "react";
 import "./transaction.css";
+import DataTable, { createTheme } from 'react-data-table-component';
+import data from './data.json';
 
 import SearchBar from "../../searchBar/SearchBar";
 
+createTheme('solarized', {
+  text: {
+    primary: '#FFFFFF',
+    secondary: '#FFFFFF',
+  },
+  background: {
+    default: '#343c63',
+  },
+  context: {
+    background: '#558BED',
+    text: '#FFFFFF',
+  },
+  divider: {
+    default: 'rgba(135, 143, 188, 0.2);',
+  },
+  action: {
+    button: 'rgba(0,0,0,.54)',
+    hover: '#558BED',
+    disabled: 'rgba(0,0,0,.12)',
+  },
+});
+
 export default function TransactionTable() {
+  const columns = [
+    {
+      name: "Date",
+      selector: "date",
+      sortable: true,
+    },
+    {
+      name: "Time",
+      selector: "time",
+      sortable: true,
+    },
+    {
+      name: "Transaction ID",
+      selector: "trnsID",
+      sortable: true,
+    },
+    {
+      name: "Value",
+      selector: "value",
+    },
+    {
+      name: "Type",
+      selector: "type",
+    },
+  ];
+
   return (
     <div className="transaction-table-wrap">
       <div className="table-top">
@@ -12,6 +62,19 @@ export default function TransactionTable() {
           <SearchBar />
         </div>
       </div>
+      <DataTable
+        columns={columns}
+        data={data}
+        highlightOnHover
+        theme={"solarized"}
+        pagination
+        paginationPerPage={5}
+        paginationRowsPerPageOptions={[5, 10, 20]}
+        paginationComponentOptions={{
+          rowsPerPageText: "Records per page:",
+          rangeSeparatorText: "out of",
+        }}
+      />
     </div>
   );
 }
